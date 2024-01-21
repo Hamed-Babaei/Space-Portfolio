20b27b88d32246cff2092e258147dafef7a19d19
Hamed Babaei <dev.hamedbabaei@gmail.com>
2024-01-22 02:47:45 +0330

    Header Page is Developed

---
+++ my-portflo/components/template/main/Hero.js
@@ -0,0 +1,18 @@
+import HeroContent from "@/components/modules/HeroContent/HeroContent";
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="relative flex flex-col h-full w-full">
+      <video
+        autoPlay
+        muted
+        loop
+        className=" rotate-180 absolute top-[-430px] left-0 z-[1] w-full h-full object-cover"
+      >
+        <source src="/blackhole.webm" type="video/webm" />
+      </video>
+      <HeroContent />
+    </div>
+  );
+}
